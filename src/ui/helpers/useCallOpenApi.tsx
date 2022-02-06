7adbceb158212b61c027a1658c8d7ce41d3ca8f6
andrei <andreigec@hotmail.com>
2022-02-06 13:21:43 +1100

    w

--- src/ui/helpers/useCallOpenApi.tsx
+++ src/ui/helpers/useCallOpenApi.tsx
@@ -3,11 +3,14 @@ import { ICallOpenApi } from './types';
 import { useEffect, useState } from 'react';
 import { callOpenApi } from './callOpenApi';
 import { CacheItems } from './routes';
+import NodeCache from 'node-cache';
 
 type AxiosWrapperWrap<T> = AxiosWrapper<T | undefined> & {
   loaded: boolean;
   loadcount: number;
 };
+
+let useCallOpenApiCache: NodeCache | undefined;
 
 export const useCallOpenApi = <T, TDefaultApi>(
   p: ICallOpenApi<T, TDefaultApi> & {
@@ -16,6 +19,10 @@ export const useCallOpenApi = <T, TDefaultApi>(
      * will shortcut and return the appropriate axioswrapper data if cachekey is found
      */
     ssrCacheItems?: CacheItems;
+    /**
+     * default ttl in seconds for cache - default 120s
+     */
+    cacheTtl?: number;
   },
 ): AxiosWrapperWrap<T> => {
   const ssrCached = p.ssrCacheItems?.find((s) => s.cacheKey === p.cacheKey);
@@ -37,9 +44,20 @@ export const useCallOpenApi = <T, TDefaultApi>(
 
   useEffect(() => {
     async function run() {
-      const res = await callOpenApi(p);
+      if (!useCallOpenApiCache) {
+        useCallOpenApiCache = new NodeCache({ stdTTL: p.cacheTtl || 120 });
+      }
+
+      let cached =
+        useCallOpenApiCache.get<AxiosWrapper<T>>(p.cacheKey) || undefined;
+
+      if (!cached) {
+        cached = await callOpenApi(p);
+        useCallOpenApiCache.set(p.cacheKey, cached);
+      }
+
       setData((d) => ({
-        ...res,
+        ...(cached as AxiosWrapper<T>),
         loaded: true,
         loading: false,
         loadcount: d.loadcount + 1,
