633d5328c8761ce8a5d5f3deab15f893b41706fe
andrei gec <andreigec@hotmail.com>
2022-04-03 09:53:14 +0800

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+exports.default = {
+  stories: [
+    "../src/story/**/*.mdx",
+    "../src/story/**/*.(ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
---
+++ src/story/Button/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '../../ui/components/Button';
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  invert: false,
+  children: 'hello world',
+};
