537dd7267562f5ef1c2598978d11e7785fcbf291
andrei <andreigec@hotmail.com>
2022-04-20 13:26:54 +1000

    w

--- src/ui/helpers/callOpenApi/hook.tsx
+++ src/ui/helpers/callOpenApi/hook.tsx
@@ -13,41 +13,50 @@ export type TUseCallOpenApi<T> = AxiosWrapper<T> & {
   setData: (d: TUseCallOpenApiDispatch<T | undefined>) => void;
 };
 
+type TUseCallOpenApi1<T, TDefaultApi> = ICallOpenApi<T, TDefaultApi> & {
+  cacheKey: string;
+  /**
+   * will shortcut and return the appropriate axioswrapper data if cachekey is found
+   */
+  ssrCacheItems?: CacheItems;
+  /**
+   * default ttl in seconds for cache - default 120s
+   */
+  cacheTtl?: number;
+};
+
+const defaultState = <T, TDefaultApi>(p: TUseCallOpenApi1<T, TDefaultApi>) => {
+  const cachedData = callOpenApiCachedRaw({ ...p, onlyCached: true })?.data;
+
+  return {
+    data: undefined,
+    url: '',
+    datetime: 0,
+    loadcount: 0,
+    loading: false,
+    reFetch: async () => {},
+    setData: () => {},
+    ...(cachedData && { data: cachedData }),
+    loaded: !!cachedData,
+  };
+};
+
 /**
  * hooks+cached call to callOpenApi
  * @param p
  * @returns
  */
 export const useCallOpenApi = <T, TDefaultApi>(
-  p: ICallOpenApi<T, TDefaultApi> & {
-    cacheKey: string;
-    /**
-     * will shortcut and return the appropriate axioswrapper data if cachekey is found
-     */
-    ssrCacheItems?: CacheItems;
-    /**
-     * default ttl in seconds for cache - default 120s
-     */
-    cacheTtl?: number;
-  },
+  pIn: TUseCallOpenApi1<T, TDefaultApi>,
 ): TUseCallOpenApi<T> => {
-  const defv: TUseCallOpenApi<T> = {
-    data: undefined,
-    url: '',
-    datetime: 0,
-    loadcount: 0,
-    loading: false,
-    loaded: false,
-    reFetch: async () => {},
-    setData: () => {},
-  };
-
-  const cachedData = callOpenApiCachedRaw({ ...p, onlyCached: true })?.data;
-  const [data, setData] = useState<TUseCallOpenApi<T>>({
-    ...defv,
-    ...(cachedData && { data: cachedData }),
-    loaded: !!cachedData,
-  });
+  const [p, setP] = useState<TUseCallOpenApi1<T, TDefaultApi>>(pIn);
+  const [data, setData] = useState<TUseCallOpenApi<T>>(defaultState(p));
+  useEffect(() => {
+    if (JSON.stringify(p) !== JSON.stringify(pIn)) {
+      setP(pIn);
+      setData(defaultState(pIn));
+    }
+  }, [p, pIn]);
 
   useEffect(() => {
     async function run() {
@@ -77,9 +86,7 @@ export const useCallOpenApi = <T, TDefaultApi>(
 
   return {
     ...data,
-    reFetch: async () => {
-      setData(defv);
-    },
+    reFetch: async () => setData(defaultState(p)),
     setData: (d) => {
       setData({ ...data, data: d(data.data), datetime: new Date().getTime() });
     },
