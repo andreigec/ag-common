1f6a9708005edba4aa3f6e79e1bf290bc5a6fbc0
andrei <andreigec@hotmail.com>
2022-12-27 14:48:15 +1100

    sw

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -3,7 +3,6 @@ export * from './Button';
 export * from './Chevron';
 export * from './Close';
 export * from './Confirm';
-export * from './DashboardAuthValidation';
 export * from './DropdownList';
 export * from './FlexColumn';
 export * from './FlexRow';
