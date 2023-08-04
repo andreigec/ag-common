7dd9217b4fded387da12776626922a7dfc74a7e4
andrei <andreigec@hotmail.com>
2023-08-04 10:40:28 +1000

    w

--- src/api/helpers/sts.ts
+++ src/api/helpers/sts.ts
@@ -18,9 +18,19 @@ export async function assumeRole({
   region,
 }: {
   assumeRoleArn: string;
-  /** assume this role arn. remember to use the credentials returned in subsequent calls */
+  /** assume this role arn. */
   region: string;
-}) {
+}): Promise<
+  | { error: string }
+  | {
+      data: {
+        accessKeyId: string;
+        secretAccessKey: string;
+        sessionToken: string;
+        expiration: Date;
+      };
+    }
+> {
   try {
     info(`assuming:${assumeRoleArn} in region:${region}`);
     const data = await sts.send(
@@ -32,6 +42,14 @@ export async function assumeRole({
 
     if (!data.Credentials) {
       return { error: 'aws assume role error' };
+    }
+    if (
+      !data.Credentials.AccessKeyId ||
+      !data.Credentials.SecretAccessKey ||
+      !data.Credentials.SessionToken ||
+      !data.Credentials.Expiration
+    ) {
+      return { error: 'missing cred values error' };
     }
 
     return {
