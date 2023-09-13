0d2fca6876dc53b3f7d9c3f12a44315aaf22b705
Andrei Gec <andreigec@hotmail.com>
2023-09-13 23:08:41 +1000

    w

--- src/ui/helpers/routesnext.ts
+++ src/ui/helpers/routesnext.ts
@@ -4,12 +4,17 @@ import { getRenderLanguage } from './routes';
 /** next13 server only */
 export const getNextAppRequest = ({
   headers,
-  searchParams: query,
 }: {
-  searchParams: Record<string, string>;
   /** use next/headers() */
   headers: { get: (s: string) => string | null };
 }) => {
+  let searchParams: Record<string, string> = {};
+  if (headers.get('x-invoke-query')) {
+    searchParams = JSON.parse(
+      decodeURIComponent(headers.get('x-invoke-query') ?? '{}'),
+    );
+  }
+
   const userAgent = headers.get('user-agent')?.toLowerCase() ?? '';
   const host = headers.get('host') ?? '';
   const pathname = headers.get('x-invoke-path') ?? '/';
@@ -17,10 +22,15 @@ export const getNextAppRequest = ({
     host.includes(':443') || !host.includes(':') ? 'https:' : 'http:';
 
   let url = `${protocol}${host}${pathname}`;
-  if (Object.keys(query).length > 0) {
-    const qs = '?' + objectToString(query, '=', '&');
+  if (Object.keys(searchParams).length > 0) {
+    const qs = '?' + objectToString(searchParams, '=', '&');
     url += qs;
   }
 
-  return { url: new URL(url), query, userAgent, lang: getRenderLanguage(host) };
+  return {
+    url: new URL(url),
+    query: searchParams,
+    userAgent,
+    lang: getRenderLanguage(host),
+  };
 };
