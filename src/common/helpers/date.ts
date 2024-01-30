6c661e1e492ceda46088b329ce61553ab1d91c2f
Andrei Gec <andreigec@hotmail.com>
2024-01-30 16:55:45 +1100

    w

--- src/common/helpers/date.ts
+++ src/common/helpers/date.ts
@@ -65,3 +65,17 @@ export const dateTimeToNearestMinute = (minutes: number, date?: Date) => {
 
   return rounded;
 };
+
+export const getUtcDateTime = () => {
+  const date = new Date();
+
+  const year = date.getUTCFullYear();
+  const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+  const day = ('0' + date.getUTCDate()).slice(-2);
+  const hours = ('0' + date.getUTCHours()).slice(-2);
+  const minutes = ('0' + date.getUTCMinutes()).slice(-2);
+  const seconds = ('0' + date.getUTCSeconds()).slice(-2);
+
+  const datetime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} UTC`;
+  return new Date(datetime);
+};
