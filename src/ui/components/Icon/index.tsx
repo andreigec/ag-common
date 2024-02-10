aebaeed163afdcae27a8f22ebb29345d18b4f4f2
Andrei Gec <andreigec@hotmail.com>
2024-02-10 22:45:11 +1100

    w

--- src/ui/components/Icon/index.tsx
+++ src/ui/components/Icon/index.tsx
@@ -49,6 +49,7 @@ export const IconF = styled.span`
     width: 100%;
     height: 100%;
     object-fit: contain;
+    aspect-ratio: 1/1;
   }
 
   img {
--- src/ui/icons/index.tsx
+++ src/ui/icons/index.tsx
@@ -2,11 +2,13 @@ import { Checkmark } from './Checkmark';
 import { ChevronRight } from './ChevronRight';
 import { Circle } from './Circle';
 import { CircleDot } from './CircleDot';
+import { Cog } from './Cog';
 import { Computer } from './Computer';
 import { CrossIcon } from './CrossIcon';
 import { Door } from './Door';
 import { Hamburger } from './Hamburger';
 import { HorizontalDots } from './HorizontalDots';
+import { House } from './House';
 import { Magnify } from './Magnify';
 import { Moon } from './Moon';
 import { Pencil } from './Pencil';
@@ -17,6 +19,7 @@ import { UserOutline } from './UserOutline';
 import { Warning } from './Warning';
 
 const icons = {
+  Cog,
   Checkmark,
   ChevronRight,
   Circle,
@@ -25,6 +28,7 @@ const icons = {
   CrossIcon,
   Door,
   Hamburger,
+  House,
   HorizontalDots,
   Magnify,
   Moon,
@@ -41,11 +45,13 @@ export {
   ChevronRight,
   Circle,
   CircleDot,
+  Cog,
   Computer,
   CrossIcon,
   Door,
   Hamburger,
   HorizontalDots,
+  House,
   icons,
   Magnify,
   Moon,
